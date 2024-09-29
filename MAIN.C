82a576e2fdddb6e737ceeebd669480d3f24bb181
mohadkaif122344@gmail.com <mohadkaif122344>
2024-09-30 01:51:04 +0530

    first comment

---
+++ MAIN.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    int sum;
+
+    // sum of two number
+    printf("enter number a :");
+    scanf("%d",&a);
+    printf("enter number b :");
+    scanf("%d",&b);
+
+    sum = a + b;
+    printf("the sum of a and b is : %d;", sum);
+
+    return 0;
+}
