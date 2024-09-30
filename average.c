9691ddf403468742f01b1c7916a39e878c4304fa
mohadkaif122344@gmail.com <mohadkaif122344>
2024-09-30 14:40:50 +0530

    message

---
+++ average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    int avg;
+
+    // average of a and b
+
+    printf("enter number a :");
+    scanf("%d",&a);
+    printf("enter number b :");
+    scanf("%d",&b);
+
+    avg = (a + b)/2;
+   printf("average number a and b is :%d ;", avg);
+    return 0;
+}
