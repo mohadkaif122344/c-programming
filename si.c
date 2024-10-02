bec151a0bb9f874d7fe32a27272287707bf8e61a
mohadkaif122344@gmail.com <mohadkaif122344>
2024-10-02 11:01:20 +0530

    kaif

---
+++ si.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+
+  int p ,r ,t ;
+   int SI;
+
+ printf("enter p :");
+ scanf("%d",&p);
+
+ printf("enter r :");
+ scanf("%d",&r);
+
+ printf("enter t :");
+ scanf("%d",&t);
+
+ SI = (p*r*t)/100;
+
+ printf("simple intrest is : %d ;", SI);
+
+
+    return 0;
+ }
