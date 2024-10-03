582543a31024c4e530e1852110ade7315994f413
mohadkaif122344@gmail.com <mohadkaif122344>
2024-10-03 18:33:40 +0530

    illustrating the switch statement

---
+++ example.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+
+int n;
+
+printf("month no :->");
+scanf("%d",&n);
+switch(n)
+{
+    case 1: 
+       printf("january\n");
+       break;
+    case 2:    
+       printf("february\n");
+       break;
+    case 3:    
+       printf("march\n");
+       break;
+    case 4:    
+       printf("april\n");
+       break;
+    case 5:    
+       printf("may\n");
+       break;
+    case 6:    
+       printf("june\n");
+       break;
+    case 7:    
+       printf("july\n");
+       break;
+    case 8:    
+       printf("august\n");
+       break;
+    case 9:    
+       printf("september\n");
+       break;
+    case 10:    
+       printf("october\n");
+       break;
+    case 11:    
+       printf("november\n");
+       break;
+    case 12:    
+       printf("december\n");
+       break;
+    default:
+       printf("invalid month number\n please try again.....\n");
+       break;
+}
+
+    return 0;
+}
