d7d00ccc5cd8cf56d9f96832a6052dc831bcbf41
mohadkaif122344@gmail.com <mohadkaif122344>
2024-11-23 22:12:09 +0530

    sizeof  program

---
+++ New folder/06.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of long long: %lu bytes\n", sizeof(long long));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of long double: %lu bytes\n", sizeof(long double));
+    printf("Size of unsigned char: %lu bytes\n", sizeof(unsigned char));
+    printf("Size of unsigned int: %lu bytes\n", sizeof(unsigned int));
+    printf("Size of unsigned short: %lu bytes\n", sizeof(unsigned short));
+    printf("Size of unsigned long: %lu bytes\n", sizeof(unsigned long));
+    printf("Size of unsigned long long: %lu bytes\n", sizeof(unsigned long long));
+
+    return 0;
+}
